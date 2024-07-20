963d4a265ddec0a7740df5858a794669e2be62e6
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2024-07-20 15:29:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	if(key=="red" | key=="white")
+	{
+		return true;
+	}
+	else{
+		return false;
+	}
 	return sampleObject.hasOwnProperty(key);
 }
 
